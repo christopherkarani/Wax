68cf57af176b1d9e64c21b8f31a8325ba7978acd
christopherkarani <chrisbkarani@gmail.com>
2026-09-26 20:26:13 +0300

    release: waxmcp v0.1.50

--- Sources/WaxCLI/WaxCLICommand.swift
+++ Sources/WaxCLI/WaxCLICommand.swift
@@ -9,7 +9,7 @@ struct WaxCLI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "wax-cli",
         abstract: "Wax developer CLI",
-        version: "0.1.49",
+        version: "0.1.50",
         subcommands: [
             RememberCommand.self,
             RecallCommand.self,
